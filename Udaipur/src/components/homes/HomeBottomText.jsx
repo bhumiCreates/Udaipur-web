2003fc7070b78c7a9be8139255ae6e01a7e30371
bhumiCreates <bhumiwadhwani.work@gmail.com>
2026-05-26 16:06:18 +0530

    Updated the home page

--- Udaipur/src/components/homes/HomeBottomText.jsx
+++ Udaipur/src/components/homes/HomeBottomText.jsx
@@ -3,12 +3,12 @@ import {Link} from 'react-router-dom'
 
 const HomeBottomText = () => {
   return (
-    <div className='font-[font1] pb-[10vh] flex items-center justify-center gap-4'>
-      <div className='border hover:border-yellow-400 hover:scale-110 transition-all duration-150 ease-out px-4 pt-5 pb-3 rounded-bl-2xl  rounded-tr-2xl overflow-hidden  uppercase leading-[2vw]'>
-      <Link className='text-[3vw]' to='/Tourism'>Tourism</Link>
+    <div className='font-[font1] pb-[10vh] flex items-center justify-center gap-8'>
+      <div className='border hover:border-yellow-400 hover:text-yellow-400 hover:scale-110 transition-all duration-150 ease-out px-4 pt-8 pb-3 rounded-bl-2xl  rounded-tr-2xl overflow-hidden  uppercase leading-[2vw]'>
+      <Link className='text-[5vw]' to='/Tourism'>Tourism</Link>
       </div>
-      <div className='border hover:border-yellow-400 hover:scale-110 transition-all duration-150 ease-out px-4 pt-5 pb-3  rounded-bl-2xl  rounded-tr-2xl overflow-hidden  uppercase leading-[2vw]'>
-      <Link className='text-[3vw]' to='/Events'>Events</Link>
+      <div className='border hover:border-yellow-400 hover:text-yellow-400 hover:scale-110 transition-all duration-150 ease-out px-4 pt-8 pb-3  rounded-bl-2xl  rounded-tr-2xl overflow-hidden  uppercase leading-[2vw]'>
+      <Link className='text-[5vw]' to='/Events'>Events</Link>
       </div>
     </div>
   )
--- Udaipur/src/components/homes/HomeHeroText.jsx
+++ Udaipur/src/components/homes/HomeHeroText.jsx
@@ -4,14 +4,14 @@ import Video from "./Video";
 const HomeHeroText = () => {
   return (
     <div className="font-[font1] pt-5">
-      <div className="text-[7vw] uppercase justify-center flex items-center leading-[10vh]">
+      <div className="text-[8vw] uppercase justify-center flex items-center leading-[10vh]">
         The Lake
-        <div className="h-[10vw] w-[20vw] rounded-bl-4xl mb-2  rounded-tr-4xl overflow-hidden justify-center flex items-center">
+        <div className="h-[12vw] w-[20vw] rounded-bl-4xl mb-2  rounded-tr-4xl overflow-hidden justify-center flex items-center m-3">
           <Video />
         </div>
         City
       </div>
-      <div className="text-[7vw] justify-center mt-2 flex items-center uppercase leading-[10vh]">
+      <div className="text-[8vw] text-yellow-400 justify-center mt-4 flex items-center uppercase leading-[10vh]">
         Udaipur
       </div>
     </div>
